aa13fda1a7f6c2065da8956785a1c3ca4b27882d
Tanguy Gimenez <gimeneztanguy@gmail.com>
2024-11-03 22:14:24 +0100

    adding main function in main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from src.ImageProcessor import ImageProcessor
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: python main.py <folder_path> <image_size>")
-    sys.exit(1)
 
-image_path = sys.argv[1]
-image_size = int(sys.argv[2])
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <folder_path> <image_size>")
+        sys.exit(1)
 
-processor = ImageProcessor(sys.argv[1])
-processor.process_folder(image_size)
+    image_path = sys.argv[1]
+    image_size = int(sys.argv[2])
+
+    processor = ImageProcessor(sys.argv[1])
+    processor.process_folder(image_size)
+
+
+if __name__ == '__main__':
+    main()
